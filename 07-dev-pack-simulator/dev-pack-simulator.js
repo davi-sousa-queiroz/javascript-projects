af044854350d9c5c4703001137928441e1acb2d9
Davi Queiroz <davisousaqueiroz26@proton.me>
2026-08-03 20:04:21 -0300

    add dev card objects to javascript.

---
+++ 07-dev-pack-simulator/dev-pack-simulator.js
@@ -0,0 +1,53 @@
+// QUERY SELECTING //
+const action = document.querySelector('.')
+const location = document.querySelector('.')
+const collectionButtton = document.querySelector('.')
+const openPackButton = document.querySelector('.')
+const displayPanel = document.querySelector('.')
+// DEV OBJECTS //
+const kamal = {
+    name: 'Kamal',
+    rarity: 'Common',
+    language: 'Python',
+    country: 'India',
+    job: 'Junior Developer',
+    salary: 40000,
+    power: 30
+}
+const john = {
+    name: 'John',
+    rarity: 'Common',
+    language: 'Javascript',
+    country: 'Australia',
+    job: 'Junior Developer',
+    salary: 60000,
+    power: 32
+}
+const lorenzo = {
+    name: 'Lorenzo',
+    rarity: 'Common',
+    language: 'Python',
+    country: 'Brazil',
+    job: 'Data Analyst',
+    salary: 50000,
+    power: 39
+}
+const emma = {
+    name: 'Emma',
+    rarity: 'Common',
+    language: 'Python',
+    country: "Russia",
+    job: 'Data Analyst',
+    salary: 60000,
+    power: 36
+}
+const nate = {
+    name: 'Nate',
+    rarity: 'Common',
+    language: 'Javascript',
+    country: 'USA',
+    job: 'Junior Developer',
+    salary: 72000,
+    power: 43
+}
+// OPEN PACK //
