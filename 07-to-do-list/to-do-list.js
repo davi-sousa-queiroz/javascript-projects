6aface885ed8df5c1427c0c6cd0ef6f69cf339c5
Davi Queiroz <davisousaqueiroz26@proton.me>
2026-08-22 17:10:09 -0300

    add base files and preparation

---
+++ 07-to-do-list/to-do-list.js
@@ -0,0 +1,22 @@
+/*
+
+PROJECT: Simple To-Do List
+
+REQUIRED:
+
+[ ] Text input
+[ ] "Add Task" button
+[ ] Task list
+
+FUNCTIONALITY:
+
+1. User types a task.
+2. Empty/space-only tasks cannot be added.
+3. Clicking Add puts the task on the page.
+4. Clicking a task marks it completed somehow.
+5. Each task has a way to be deleted.
+6. Deleted tasks disappear from the page.
+7. Try to keep your JavaScript organized with functions.
+
+*/
+
