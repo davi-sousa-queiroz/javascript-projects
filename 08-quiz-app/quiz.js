7c76d573c9802e364df7d03dac4b73e3d6158a6e
Davi Queiroz <davisousaqueiroz26@proton.me>
2026-08-28 15:10:43 -0300

    add all questions and answers.

--- 08-quiz-app/quiz.js
+++ 08-quiz-app/quiz.js
@@ -4,13 +4,54 @@ const playButton = document.querySelector('.play-button')
 const questions = [
     {
         question: 'What does DOM stand for?',
+
         answers: [
             'Document Object Model',
             'Data Object Method',
             'Digital Order Manager',
             'Document Order Model'
         ],
+
         correctAnswer: 'Document Object Model'
+    },
+
+    {
+        question: 'Which JavaScript method is used to select an element from the page?',
+
+        answers: [
+            'document.querySelector()',
+            'document.createElement()',
+            'document.appendChild()',
+            'document.getText()'
+        ],
+
+        correctAnswer: 'document.querySelector()'
+    },
+
+    {
+        question: 'What does the push() method do to an array?',
+
+        answers: [
+            'Adds an item to the end',
+            'Removes the first item',
+            'Sorts the array',
+            'Returns the array length'
+        ],
+
+        correctAnswer: 'Adds an item to the end'
+    },
+
+    {
+        question: 'What does addEventListener() allow you to do?',
+
+        answers: [
+            'Respond to events like clicks',
+            'Create an array',
+            'Style an HTML element',
+            'Create a new JavaScript file'
+        ],
+
+        correctAnswer: 'Respond to events like clicks'
     }
 ]
 
