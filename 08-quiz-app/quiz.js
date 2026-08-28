1ebd9e5e0f1d1115934fc71f097e78970c3d0032
Davi Queiroz <davisousaqueiroz26@proton.me>
2026-08-28 14:51:12 -0300

    add javascript variables, query selections, base functions, and questions array and related objects.

--- 08-quiz-app/quiz.js
+++ 08-quiz-app/quiz.js
@@ -1,2 +1,24 @@
 const heroHeading = document.querySelector('.hero-heading')
+<<<<<<< HEAD
 const playButton = document.querySelector('.play-button')
+=======
+const playButton = document.querySelector('.play-button')
+
+const questions = [
+    {
+        question: 'What does DOM stand for?',
+        answers: [
+            'Document Object Model',
+            'Data Object Method',
+            'Digital Order Manager',
+            'Document Order Model'
+        ],
+        correctAnswer: 'Document Object Model'
+    }
+]
+
+function gameStart () {
+
+}
+
+playButton.addEventListener('click', gameStart)
