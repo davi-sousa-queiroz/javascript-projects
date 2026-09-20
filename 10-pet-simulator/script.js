828fd3ccd6afcea49ad4635e7d1f72126a75dddc
Davi Queiroz <davisousaqueiroz26@proton.me>
2026-09-20 20:23:22 -0300

    add stat updating system + DOM manipulation.

--- 10-pet-simulator/script.js
+++ 10-pet-simulator/script.js
@@ -4,31 +4,46 @@ const happyStat = document.querySelector('#happy-val')
 const energyStat = document.querySelector('#energy-stat')
 const dayCount = document.querySelector('#day-val')
 
+const feed = document.querySelector('.btn-feed')
+const play = document.querySelector('.btn-play')
+const sleep = document.querySelector('.btn-sleep')
+
 const pet = {
     maxStat: 10,
     hunger: 8,
     happy: 5,
     energy: 9,
+
+    updateStats(hunger, happy, energy) {
+        hungerStat.textContent = hunger
+        happyStat.textContent = happy
+        energyStat.textContent = energy
+    },
     
     newDay() {
         this.hunger = Math.max(0, this.hunger - 4);
         this.happy = Math.max(0, this.happy - 3);
         this.energy = Math.max(0, this.energy - 1);
+        this.updateStats(this.hunger, this.happy, this.energy)
     },
 
     feed() {
         this.hunger = Math.min(this.maxStat, this.hunger + 3);
         this.energy = Math.min(this.maxStat, this.energy + 1);
+        this.updateStats(this.hunger, this.happy, this.energy)
     },
 
     play() {
         this.happy = Math.min(this.maxStat, this.happy + 3);
         this.energy = Math.max(0, this.energy - 2);
         this.hunger = Math.max(0, this.hunger - 1);
+        this.updateStats(this.hunger, this.happy, this.energy)
     },
 
     sleep() {
         this.energy = Math.min(this.maxStat, this.energy + 5); 
         this.hunger = Math.max(0, this.hunger - 2);
+        this.updateStats(this.hunger, this.happy, this.energy)
     }
 }
+
