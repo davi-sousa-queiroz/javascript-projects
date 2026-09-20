b6fb7506d19abcb48c7172cbb5a1a44be851957b
Davi Queiroz <davisousaqueiroz26@proton.me>
2026-09-20 14:47:33 -0300

    add query selectors.

---
+++ 10-pet-simulator/script.js
@@ -0,0 +1,5 @@
+const gameBackground = document.querySelector('.game-background')
+const hungerStat = document.querySelector('#hunger-val')
+const happyStat = document.querySelector('#happy-val')
+const energyStat = document.querySelector('#energy-stat')
+const dayCount = document.querySelector('#day-val')
