29c3b40657ee672f9969efe84b4a1757b9c32e5d
Davi Queiroz <davisousaqueiroz26@proton.me>
2026-09-20 15:57:10 -0300

    add pet object and basic stats.

--- 10-pet-simulator/script.js
+++ 10-pet-simulator/script.js
@@ -3,3 +3,10 @@ const hungerStat = document.querySelector('#hunger-val')
 const happyStat = document.querySelector('#happy-val')
 const energyStat = document.querySelector('#energy-stat')
 const dayCount = document.querySelector('#day-val')
+
+const pet = {
+    maxStat: 10,
+    hunger: 8,
+    happy: 5,
+    energy: 9
+}
