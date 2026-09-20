2f224252095c2c0aa1535bf724b0bc900f855d51
Davi Queiroz <davisousaqueiroz26@proton.me>
2026-09-20 19:35:47 -0300

    add object methods for each action

--- 10-pet-simulator/script.js
+++ 10-pet-simulator/script.js
@@ -8,5 +8,27 @@ const pet = {
     maxStat: 10,
     hunger: 8,
     happy: 5,
-    energy: 9
+    energy: 9,
+    
+    newDay() {
+        this.hunger = Math.max(0, this.hunger - 4);
+        this.happy = Math.max(0, this.happy - 3);
+        this.energy = Math.max(0, this.energy - 1);
+    },
+
+    feed() {
+        this.hunger = Math.min(this.maxStat, this.hunger + 3);
+        this.energy = Math.min(this.maxStat, this.energy + 1);
+    },
+
+    play() {
+        this.happy = Math.min(this.maxStat, this.happy + 3);
+        this.energy = Math.max(0, this.energy - 2);
+        this.hunger = Math.max(0, this.hunger - 1);
+    },
+
+    sleep() {
+        this.energy = Math.min(this.maxStat, this.energy + 5); 
+        this.hunger = Math.max(0, this.hunger - 2);
+    }
 }
